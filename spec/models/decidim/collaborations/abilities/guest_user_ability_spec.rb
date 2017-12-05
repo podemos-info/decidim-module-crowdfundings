4d8c0ebdfe85d3a499e987532a4519ce51bf1fd7
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-12-05 15:42:11 +0100

    Changes after rebasing with master
    Removed all references to donate and its derivatives after rebasing with master.

--- spec/models/decidim/collaborations/abilities/guest_user_ability_spec.rb
+++ spec/models/decidim/collaborations/abilities/guest_user_ability_spec.rb
@@ -16,32 +16,32 @@ describe Decidim::Collaborations::Abilities::GuestUserAbility do
                                   .and_return(collaborations_allowed)
   end
 
-  context 'donate to collaboration' do
+  context 'support collaboration' do
     context 'collaboration allowed' do
       let(:collaborations_allowed) { true }
 
-      it 'let the user donate when collaboration accepts donations' do
-        expect(collaboration).to receive(:accepts_donations?).and_return(true)
-        expect(subject).to be_able_to(:donate, collaboration)
+      it 'when collaboration accepts supports' do
+        expect(collaboration).to receive(:accepts_supports?).and_return(true)
+        expect(subject).to be_able_to(:support, collaboration)
       end
 
-      it 'Do not let the user donate when collaboration do not accepts donations' do
-        expect(collaboration).to receive(:accepts_donations?).and_return(false)
-        expect(subject).not_to be_able_to(:donate, collaboration)
+      it 'when collaboration do not accepts supports' do
+        expect(collaboration).to receive(:accepts_supports?).and_return(false)
+        expect(subject).not_to be_able_to(:support, collaboration)
       end
     end
 
     context 'collaboration not allowed' do
       let(:collaborations_allowed) { false }
 
-      it 'do not let the user donate when collaboration accepts donations' do
-        expect(collaboration).to receive(:accepts_donations?).and_return(true)
-        expect(subject).not_to be_able_to(:donate, collaboration)
+      it 'when collaboration accepts supports' do
+        expect(collaboration).to receive(:accepts_supports?).and_return(true)
+        expect(subject).not_to be_able_to(:support, collaboration)
       end
 
-      it 'Do not let the user donate when collaboration do not accepts donations' do
-        expect(collaboration).to receive(:accepts_donations?).and_return(false)
-        expect(subject).not_to be_able_to(:donate, collaboration)
+      it 'when collaboration do not accepts supports' do
+        expect(collaboration).to receive(:accepts_supports?).and_return(false)
+        expect(subject).not_to be_able_to(:support, collaboration)
       end
     end
   end
