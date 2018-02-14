f46dab14b78e5d95297a38ff21e5574b01d16732
David Rodríguez <deivid.rodriguez@riseup.net>
2018-02-14 20:55:53 -0300

    Remove dummy app from source control

--- spec/decidim_dummy_app/db/migrate/20171205103726_add_terms_and_conditions_to_decidim_collaborations_collaborations.decidim_collaborations.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from decidim_collaborations (originally 20171124111736)
-class AddTermsAndConditionsToDecidimCollaborationsCollaborations < ActiveRecord::Migration[5.1]
-  def change
-    add_column :decidim_collaborations_collaborations, :terms_and_conditions, :jsonb
-  end
-end
