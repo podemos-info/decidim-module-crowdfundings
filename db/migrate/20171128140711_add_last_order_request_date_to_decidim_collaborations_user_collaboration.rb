d5aebf9862fcfa29e7172a87b03305597cd517ba
David Rodríguez <deivid.rodriguez@riseup.net>
2018-02-14 22:13:09 -0300

    Fix development application seeding

--- db/migrate/20171128140711_add_last_order_request_date_to_decidim_collaborations_user_collaboration.rb
+++ db/migrate/20171128140711_add_last_order_request_date_to_decidim_collaborations_user_collaboration.rb
@@ -1,8 +1,12 @@
 class AddLastOrderRequestDateToDecidimCollaborationsUserCollaboration < ActiveRecord::Migration[5.1]
+  class UserCollaboration < ApplicationRecord
+    self.table_name = :decidim_collaborations_user_collaborations
+  end
+
   def change
     add_column :decidim_collaborations_user_collaborations, :last_order_request_date, :date
 
-    Decidim::Collaborations::UserCollaboration.find_each do |collaboration|
+    UserCollaboration.find_each do |collaboration|
       collaboration.update(last_order_request_date: collaboration.created_at.beginning_of_month)
     end
   end
