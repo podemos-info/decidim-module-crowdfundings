b9a40c6bd466303ccc55611ff5c8e95ec25f0fd6
David Rodríguez <deivid.rodriguez@riseup.net>
2018-02-19 15:13:37 -0300

    Make quasi-tautological test to be less brittle
    If you read the test and the implementation, the test is basically
    replicating the implementation making it very brittle, since almost any
    change in the implementation will most likely break the test, even if
    the output of the method doesn't change.
    
    Instead, assert for the real expected values, which is also more
    readable.

--- spec/helpers/decidim/collaborations/collaborations_helper_spec.rb
+++ spec/helpers/decidim/collaborations/collaborations_helper_spec.rb
@@ -6,17 +6,15 @@ module Decidim
   module Collaborations
     describe CollaborationsHelper do
       describe "frequency_options" do
-        it "returns an option per frequency value" do
-          expect(helper.frequency_options.count).to eq(UserCollaboration.frequencies.count)
-        end
-
-        it "each option is returned with its value and a valid translation" do
-          helper.frequency_options.each do |option|
-            expect(option[0]).to eq(I18n.t(
-                                      option[1].to_sym,
-                                      scope: "decidim.collaborations.labels.frequencies"
-            ))
-          end
+        it "returns available frequency options" do
+          expect(helper.frequency_options).to eq(
+            [
+              %w(Punctual punctual),
+              %w(Monthly monthly),
+              %w(Quarterly quarterly),
+              %w(Annual annual)
+            ]
+          )
         end
       end
 
